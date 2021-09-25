c87a41f18fde0c0f0e8d8af5fd6d315318f76025
Lucas Furlanetto Pascoali <luccafpascoali@gmail.com>
2021-09-24 21:54:14 -0300

    Add files via upload

---
+++ bubble_sort.rb
@@ -0,0 +1,20 @@
+def bubble_sort(array)
+  is_sorted = false
+  changes_number = 0
+  until is_sorted do
+    for i in 0...array.length do
+      condition = array[i] <=> array[i + 1]
+      if condition == 1
+        tmp = array[i]
+        array[i] = array[i + 1]
+        array[i + 1] = tmp
+        changes_number += 1
+      end     
+    end
+    is_sorted = changes_number == 0 ? true : false
+    changes_number = 0
+  end
+  array
+end
+
+p bubble_sort([4,23,78,23,90,2, 0, 93, 76])
